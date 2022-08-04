32a6c3b846c99400c1499caa64ed0c6c36049794
Valentina <vamlopezeguren@gmail.com>
2022-08-04 09:28:46 -0300

    Orden de compra

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,9 +3,28 @@ import { Link } from 'react-router-dom';
 import ItemCart from '../containers/ItemCart';
 import {useCartContext} from "../context/CartContext";
 import "./styles/ItemCart.css"
+import {addDoc, collection, getFirestore} from "firebase/firestore";
 
 const Cart = () => {
 	const { cart, totalPrice } = useCartContext();
+
+  const order = {
+    buyer:{
+      name: "Andrea",
+      email: "andreagarcia@gmail.com",
+      phone: "095456321",
+      address: "Bv. Artigas 1234",
+    },
+    items: cart.map (product => ({id: product.id, title:product.title, price:product.price, quantity:product.quantity})),
+    total: totalPrice(),
+  }
+
+	const handleClick = () => {
+		const db = getFirestore();   
+		const ordersCollection = collection(db, "orders");
+    addDoc (ordersCollection, order)
+    .then (({id})=>console.log(id)) 
+  };
 
 if (cart.length === 0) {
   return (
@@ -22,6 +41,7 @@ return (
       <ItemCart key={product.id} product={product} />
     ))}
     <p className="total">total: USD {totalPrice()}</p>
+    <button onClick={handleClick}>Confirmar compra</button>
   </>
 );
 };
